fe436213c3bfd983df70a25c884de19760c3efd7
Arthur <arthur.barrabonita@gmail.com>
2023-11-23 21:08:23 -0300

    site marcio

---
+++ website/site/scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+});
+
+function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
+
+
+function toggleLike(icon) {
+    icon.classList.toggle('liked');
+}
+
+function toggleBookmark(icon) {
+    icon.classList.toggle('bookmarked');
+}
